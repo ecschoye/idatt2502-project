96795172e4985c396e30174eefe0aa4ede437b94
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-11-09 09:53:34 +0100

    Changed comment

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-# Makes src a installable package
-# To install the package:
-# pip install -e .
+'''
+Makes src a installable package.
+To install the package:
+pip install -e .
+'''
 setup(
     name="idatt2502-project",
     version="1.0",
