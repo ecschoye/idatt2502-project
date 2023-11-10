f61893c3d561e941471bc8ec19ba5215af3e104d
edvard <ecschoye@stud.ntnu.no>
2023-11-10 09:32:58 +0100

    modified main.py

--- src/main.py
+++ src/main.py
@@ -8,9 +8,15 @@ from training.ddqn_training import (
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Machine Learning Project Options")
-    parser.add_argument("option", nargs="?", default="ddqn", type=str, help="Select an option (ddqn, ppo, render_ddqn, render_ppo)")
-    parser.add_argument("--log", action="store_true", help="Enable logging")
-    parser.add_argument("--log-model", action="store_true", help="Enable model logging")
+    parser.add_argument("option", nargs="?", default="ddqn", type=str,
+                        help="Select an option: 'ddqn' for DDQN training, "
+                             "'ppo' for PPO training, 'render-ddqn' to render DDQN, "
+                             "'render-ppo' to render PPO, 'log-ddqn' to log DDQN model, "
+                             "'log-ppo' to log PPO model")
+    parser.add_argument("--log", action="store_true",
+                        help="Enable logging (only applicable with 'ddqn' or 'ppo')")
+    parser.add_argument("--log-model", action="store_true",
+                        help="Enable model logging (only applicable with 'ddqn' or 'ppo')")
 
     args = parser.parse_args()
 
@@ -22,15 +28,21 @@ if __name__ == "__main__":
             log_model_version()
     elif args.option == "ppo":
         print("PPO")
-        #train_ppo(log=args.log) ADD PPO TRANING
+        # train_ppo(log=args.log) ADD PPO TRANING
         if args.log_model:
             print("Log model")
-            #log_model_version() ADD PPO LOGING MODEL
+            # log_model_version() ADD PPO LOGING MODEL
     elif args.option == "render-ddqn":
         print("Rendering ddqn")
         render_mario()
     elif args.option == "render-ppo":
         print("Rendering ppo")
-        #render_ppo() ADD PPO RENDER METHOD
+        # render_ppo() ADD PPO RENDER METHOD
+    elif args.option == "log-ddqn":
+        print("Logging DDQN Model")
+        log_model_version()
+    elif args.option == "log-ppo":
+        print("Logging PPO Model")
+        # log_model_version()
     else:
         print("Invalid option. Please select an option (ddqn, ppo, render_ddqn, render_ppo).")
